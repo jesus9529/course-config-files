961979311892f297f6e5c8327d4644348c3c4bb1
Jesus Hernandez <jesus9529@gmail.com>
2024-12-18 15:58:43 +0100

    adding test angular project for didactic extension show purpose

---
+++ test/src/app/prueba/prueba.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PruebaRoutingModule } from './prueba-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PruebaRoutingModule
+  ]
+})
+export class PruebaModule { }
